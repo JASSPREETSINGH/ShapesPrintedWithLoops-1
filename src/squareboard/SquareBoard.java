96c7bce623ccd7b5e9a87c1b7a081b4bf9a9a565
Jaspreet Singh <Dell@192.168.2.239>
2018-01-01 16:46:33 -0500

    squareBoard

--- src/squareboard/SquareBoard.java
+++ src/squareboard/SquareBoard.java
@@ -15,7 +15,13 @@ public class SquareBoard {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int size = 7;    // size of the board
+      for (int row = 1; row <= size; ++row) {  // Use row and col, NOT i and j, or x and y
+         for (int col = 1; col<=size; ++col) {
+           System.out.print("# ");   // print # and a space, without newline
+         }
+        System.out.println();     // print a newline
+      } // TODO code application logic here
     }
     
 }
